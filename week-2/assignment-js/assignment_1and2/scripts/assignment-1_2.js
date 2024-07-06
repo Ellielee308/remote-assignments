e889c68374f3c5741c629bb35f7631d7c749803c
Ellie Lee <as152808@gmail.com>
2024-07-06 18:00:16 +0800

    Modify week 2 assignments

--- week-2/assignment-js/assignment_1and2/scripts/assignment-1_2.js
+++ week-2/assignment-js/assignment_1and2/scripts/assignment-1_2.js
@@ -1,26 +1,26 @@
 function max(numbers) {
-    let numberMax = numbers[0]; 
-    for (let i = 0; i < numbers.length; i++){
-        if (numbers[i] > numberMax){
-            numberMax = numbers[i];
-        }
+  let numberMax = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > numberMax) {
+      numberMax = numbers[i];
     }
-    return numberMax;
+  }
+  return numberMax;
 }
 
 // function calculate(args) {
-    function calculate(args) {
-        let calculateResult;
-        if (args.op === "+"){
-            calculateResult = args.n1 + args.n2;
-        } else if (args.op === "-"){
-            calculateResult = args.n1 - args.n2;
-        } else if (args.op === "*"){
-            calculateResult = args.n1 * args.n2;
-        } else if (args.op === "/"){
-            calculateResult = args.n1 / args.n2;
-        } else {
-            calculateResult = "Not supported";
-        }
-        return calculateResult;
-    }
+function calculate(args) {
+  let calculateResult;
+  if (args.op === "+") {
+    calculateResult = args.n1 + args.n2;
+  } else if (args.op === "-") {
+    calculateResult = args.n1 - args.n2;
+  } else if (args.op === "*") {
+    calculateResult = args.n1 * args.n2;
+  } else if (args.op === "/") {
+    calculateResult = args.n1 / args.n2;
+  } else {
+    calculateResult = "Not supported";
+  }
+  return calculateResult;
+}
---
+++ week-2/assignment-js/assignment_3and5/scripts/assignment-3_5.js
@@ -0,0 +1,22 @@
+function calculate(data) {
+  let total = 0;
+  for (let i = 0; i < data.products.length; i++) {
+    total += data.products[i].price;
+  }
+  return total * (1 - data.discount);
+}
+
+function twoSum(nums, target) {
+  let indexFirst;
+  let indexSecond;
+  for (let i = 0; i < nums.length; i++) {
+    indexFirst = i;
+    let difference = target - nums[i];
+    for (let y = i + 1; y < nums.length; y++) {
+      if (nums[y] === difference) {
+        indexSecond = y;
+        return [indexFirst, indexSecond];
+      }
+    }
+  }
+}
