8a289673a59b601a3e56034b474e8362d3655417
Ellie Lee <as152808@gmail.com>
2024-07-12 11:42:16 +0800

    Modify assignment 3

--- week-4/assignment3/src/components/Main.js
+++ week-4/assignment3/src/components/Main.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ContentSection from "./ContentSection";
 
 const Main = () => {
+  const [goodTime, setGoodTime] = useState(false);
+  const changeMessage = () => setGoodTime(!goodTime);
+
   return (
     <main>
-      <h1 id="welcome">Welcome Message</h1>
+      {goodTime ? (
+        <h1 id="welcome" onClick={changeMessage}>
+          Have a Good Time!
+        </h1>
+      ) : (
+        <h1 id="welcome" onClick={changeMessage}>
+          Welcome Message
+        </h1>
+      )}
       <ContentSection />
     </main>
   );
